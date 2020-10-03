66457649e5f3376682291c69cdced4861e757e4c
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2020-10-03 17:55:11 +1000

    fix deps

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+import * as service from './types/Service';
+import * as api from './utils/API';
+
 test('renders learn react link', () => {
 //  const { getByText } = render(<App />);
 //  const linkElement = getByText(/learn react/i);
